9f0a77c3f1df5739ea4d724888ea4ec31e7441ad
Jarred <j.mcknight.d@gmail.com>
2015-06-06 12:06:42 -0500

    added login_activity.java added loginview.xml
    edited androidManifest.xml
       changed the launcher intent

---
+++ app/src/main/java/com/example/daddyz/turtleboys/login_activity.java
@@ -0,0 +1,19 @@
+package com.example.daddyz.turtleboys;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Jinbir on 6/6/2015.
+ */
+public class login_activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loginview);
+        // Enable Local Datastore.
+
+    }
+}
