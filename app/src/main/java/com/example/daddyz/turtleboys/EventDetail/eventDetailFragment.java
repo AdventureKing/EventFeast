9d4030671b38c56e2a6ebda6c9707b3d3676fe8a
AdventureKing <basnow2010@gmail.com>
2015-07-03 22:41:52 -0500

    -adding automatically to the default calendar like a boss

--- app/src/main/java/com/example/daddyz/turtleboys/EventDetail/eventDetailFragment.java
+++ app/src/main/java/com/example/daddyz/turtleboys/EventDetail/eventDetailFragment.java
@@ -2,7 +2,10 @@ package com.example.daddyz.turtleboys.EventDetail;
 
 
 import android.app.Fragment;
+import android.content.ContentValues;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -13,8 +16,12 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.daddyz.turtleboys.R;
+
+import java.util.Calendar;
+import java.util.TimeZone;
 
 /**
  * Created by snow on 6/25/2015.
@@ -72,7 +79,31 @@ public class eventDetailFragment extends Fragment {
         eventDesc = (TextView) view.findViewById(R.id.EventDesc);
         eventDesc.setText(obj.getEventDesc());
 
+        calendarButton = (Button) view.findViewById(R.id.AddToCalendarButton);
+        calendarButton.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                // create Intent to take a picture and return control to the calling application
+             /*   Intent calIntent = new Intent(Intent.ACTION_INSERT);
+                calIntent.setType("vnd.android.cursor.item/event");
+                calIntent.putExtra(CalendarContract.Events.TITLE, "My House Party");
+                calIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "My Beach House");
+                calIntent.putExtra(CalendarContract.Events.DESCRIPTION, "A Pig Roast on the Beach");
+                //instaiate with the time to start
+                GregorianCalendar calDate = new GregorianCalendar(2015, 7, 15,12,15,10);
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                        calDate.getTimeInMillis());
+                calDate.set(2015, 7, 15,17,15,10);
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+                        calDate.getTimeInMillis());
+
+
+                startActivity(calIntent);*/
+
+                autocreate();
+            }
+        });
 
         //actionlisteners for the buttons
 
@@ -101,5 +132,25 @@ public class eventDetailFragment extends Fragment {
             frame.setVisibility(View.VISIBLE);
 
         }
+    }
+    public void autocreate(){
+        Calendar beginTime = Calendar.getInstance();
+        beginTime.set(2015, 7, 20, 7, 30);
+        Calendar endTime = Calendar.getInstance();
+        endTime.set(2015, 7, 20, 8, 30);
+
+
+        ContentValues calEvent = new ContentValues();
+        calEvent.put(CalendarContract.Events.CALENDAR_ID, 1); // XXX pick)
+        calEvent.put(CalendarContract.Events.TITLE, "title is game time");
+        calEvent.put(CalendarContract.Events.DTSTART, beginTime.getTimeInMillis());
+        calEvent.put(CalendarContract.Events.DTEND, endTime.getTimeInMillis());
+        calEvent.put(CalendarContract.Events.EVENT_TIMEZONE, TimeZone.getDefault().getID());
+        Uri uri = getActivity().getContentResolver().insert(CalendarContract.Events.CONTENT_URI, calEvent);
+
+        // The returned Uri contains the content-retriever URI for
+        // the newly-inserted event, including its id
+        int id = Integer.parseInt(uri.getLastPathSegment());
+        Toast.makeText(getActivity(), "EVENT_TITLE WAS ADDED TO THE CALENDAR", Toast.LENGTH_SHORT).show();
     }
 }
