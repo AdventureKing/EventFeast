d6cf37734ddd28d28fbceee24de5dd6197753d0c
zachrdz <zjrodr0709@hotmail.com>
2015-07-15 03:50:44 -0500

    Moving web services to new, shorter directory

---
+++ api/v1/includes.php
@@ -0,0 +1,8 @@
+<?php
+	// Classes
+	require_once($projectDir.'class/externalAPIs/stubhub/functions.php');
+	require_once($projectDir.'class/externalAPIs/ticketmaster/functions.php');
+
+	// Models
+	require_once($projectDir.'models/findEventsModel.php');
+?>
