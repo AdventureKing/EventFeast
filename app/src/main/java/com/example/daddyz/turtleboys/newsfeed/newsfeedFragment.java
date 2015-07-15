ed7d03308de3469a4290b79d2711c128183c80f8
AdventureKing <basnow2010@gmail.com>
2015-07-15 18:07:48 -0500

    -got swipe down to refresh to work time to work on post detail

--- app/src/main/java/com/example/daddyz/turtleboys/newsfeed/newsfeedFragment.java
+++ app/src/main/java/com/example/daddyz/turtleboys/newsfeed/newsfeedFragment.java
@@ -3,6 +3,7 @@ package com.example.daddyz.turtleboys.newsfeed;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -34,18 +35,29 @@ public class newsfeedFragment extends Fragment{
     private newsfeedAdapter adapter;
     private ArrayList<newsfeedObject> newsfeedList;
     private  FloatingActionButton fab;
-
+    private SwipeRefreshLayout mSwipeRefreshLayout;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         rootView = inflater.inflate(R.layout.newsfeedlistfragment, container, false);
-        list = (ListView) rootView.findViewById(R.id.listView);
+        //swipe layout
+        mSwipeRefreshLayout = (SwipeRefreshLayout) rootView.findViewById(R.id.activity_main_swipe_refresh_layout);
+        //pull down to refresh the list
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshContent();
 
+            }
+        });
+        //list
+        list = (ListView) rootView.findViewById(R.id.listView);
         mTextView = (TextView) rootView.findViewById(R.id.textView);
         mButton = (Button) rootView.findViewById(R.id.button);
         list.setClickable(true);
+
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
@@ -98,4 +110,16 @@ public class newsfeedFragment extends Fragment{
 
         return rootView;
     }
+
+
+    //refresh the list
+    private void refreshContent() {
+        adapter = new newsfeedAdapter(getActivity(), R.layout.eventfeedroweven, newsfeedList);
+        list.setAdapter(adapter);
+        //refreshing is done at this point
+        mSwipeRefreshLayout.setRefreshing(false);
+        Toast.makeText(getActivity(), "User Wants to refresh the list", Toast.LENGTH_SHORT).show();
+    }
+
+
 }
