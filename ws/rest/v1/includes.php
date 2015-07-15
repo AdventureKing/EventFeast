999d20358148c2f5e53f3fbb5333d106a6b57c2f
zachrdz <zjrodr0709@hotmail.com>
2015-07-15 04:53:35 -0500

    removing old web service directory since it is being replaced with api directory

--- ws/rest/v1/includes.php
+++
@@ -1,8 +0,0 @@
-<?php
-	// Classes
-	require_once($projectDir.'class/externalAPIs/stubhub/functions.php');
-	require_once($projectDir.'class/externalAPIs/ticketmaster/functions.php');
-
-	// Models
-	require_once($projectDir.'models/findEventsModel.php');
-?>
--- ws/rest/v1/routes.php
+++
@@ -1,5 +0,0 @@
-<?php
-	require_once($projectDir.'Routes/findEvents.php');
-	require_once($projectDir.'Routes/event.php');
-	require_once($projectDir.'Routes/events.php');
-?>
