c1e86d9cc3bed51acc064b7e4b41460b2e1f8fa2
AdventureKing <basnow2010@gmail.com>
2015-07-25 13:15:11 -0500

    -adding some standard code to the mix

--- app/src/main/java/com/example/daddyz/turtleboys/feedTabView/feedtabview.java
+++ app/src/main/java/com/example/daddyz/turtleboys/feedTabView/feedtabview.java
@@ -2,28 +2,19 @@ package com.example.daddyz.turtleboys.feedTabView;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import com.example.daddyz.turtleboys.R;
 
 /**
  * Created by snow on 7/23/2015.
  */
 public class feedtabview extends Fragment {
 
-    //need this to replace the frame in the main activity
-    private View rootView;
-
-    //just a blanket on create view
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        rootView = inflater.inflate(R.layout.feedtablayout, container, false);
 
 
-        return rootView;
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
     }
 }
